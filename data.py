ff33d4f022e12f920eb2f04d61d4ea19d2be2e77
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-09-22 12:18:00 -0400

    added data from open trivia database for historic medium level true false questions

--- data.py
+++ data.py
@@ -1,16 +1,40 @@
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home "
-             "to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state "
-             "funeral.", "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+    {
+        "category": "History", "type": "boolean", "difficulty": "medium",
+        "question": "The two atomic bombs dropped on Japan by the United States in August 1945 were named &#039;"
+        "Little Man&#039; and &#039;Fat Boy&#039;.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+    "question": "The first televised presidential debate was between Jimmy Carter and Gerald Ford.",
+    "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+    "question": "Assyrian king Sennacherib&#039;s destruction of Babylon in 689 BCE was viewed as a triumph by other"
+                " Assyrian citizens.",
+    "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+    "question": "&quot;I disapprove of what you say, but I will defend to the death your right to say it&quot; is a"
+                " quote from French philosopher Voltaire.",
+    "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+    "question": "Theodore Roosevelt Jr. was the only General involved in the initial assault on D-Day.",
+    "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+    "question": "Abraham Lincoln was the first U.S. President to be born outside the borders of the thirteen original"
+                " states. ",
+    "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+    "question": "The Hundred Years&#039; War was fought for more than a hundred years.",
+    "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+    "question": "Adolf Hitler was accepted into the Vienna Academy of Fine Arts.",
+    "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+    "question": "The Korean War ended in 1953 without any ceasefire.",
+    "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "History", "type": "boolean", "difficulty": "medium",
+    "question": "Sir Issac Newton served as a Member of Parliament, but the only recorded time he spoke was to "
+                "complain about a draft in the chambers.",
+    "correct_answer": "True", "incorrect_answers": ["False"]}
 ]
