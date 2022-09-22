9a2e23c4b39a53e114d201f623f2cb900be61b78
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-09-22 11:46:45 -0400

    capture user answer and change it to lowercase for accurate comparison

--- quiz_brain.py
+++ quiz_brain.py
@@ -4,6 +4,7 @@ class QuizBrain:
 
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
@@ -11,6 +12,11 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False):")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False):")
+        self.check_answer(user_answer, current_question.answer)
 
+    def check_answer(self, user_answer, correct_answer):
+        #change both to lower case to avoid and errors in comparison
+        if user_answer.lower() == correct_answer.lower()
 
+        self.score += 1
